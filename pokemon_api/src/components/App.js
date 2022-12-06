90e11ad015e33fad493920795a83f7cef50aaae5
FreakJazz <jazzrb2307@hotmail.com>
2022-12-05 22:00:03 -0500

    add structure

---
+++ pokemon_api/src/components/App.js
@@ -0,0 +1,14 @@
+
+import './App';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        POKEMON APP
+      </header>
+    </div>
+  );
+}
+
+export default App;
